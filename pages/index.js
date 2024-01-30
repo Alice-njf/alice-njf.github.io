d6c8208deac8301f592132d369b00d39b7bfc1d8
Alice-njf <84927344+Alice-njf@users.noreply.github.com>
2024-01-30 20:43:50 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center">
           <img className="w-32 h-32 mb-8 rounded-full" src="/katherine.jpg" alt="Katherine" />
           <h1 className="mx-auto text-2xl font-semibold tracking-widest text-center sm:text-3xl">
-            KATHERINE OELSNER
+            William Schanaei
           </h1>
           <hr className="w-16 my-8 border-gray-300" />
           <h2 className="text-lg tracking-widest text-center">SENIOR SOFTWARE ENGINEER</h2>
